4cb517fd2c1bd69207a654082e0fcdd60897725e
TechUsopp <kevin1080p.mdm@gmail.com>
2024-11-29 22:22:18 -0300

    Adicionado as classes e migração de banco criada com MYSLQ

---
+++ EtecShopAPI/Controllers/CategoriasController.cs
@@ -0,0 +1,79 @@
+using EtecShopAPI.Data;
+using EtecShopAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EtecShopAPI.Controllers
+{
+    [ApiController]
+    [Route("api/categorias")]
+    public class CategoriasController(AppDbContext db) : ControllerBase
+    {
+        private readonly AppDbContext _db = db;
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Get() => Ok(await _db.Categorias.ToListAsync());
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult> Get(int id) =>_db.Categorias.Any(c => c.Id == id) ?
+                Ok(await _db.Categorias.FindAsync(id)) :
+                NotFound("Categoria não encontrada!")
+
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult> Create([FromBody] Categoria categoria)
+        {
+         if (!ModelState.IsValid)
+            return BadRequest("Categoria informada com problemas");
+        if (_db.Categorias.Any(c => c.Nome == categoria.Nome))
+             return BadRequest($"Já existe uma Categoria com o nome: '{categoria.Nome}'");
+        await _db.Categorias.AddAsync(categoria);
+        await _db.SaveChangesAsync();
+        return CreatedAtAction(nameof(Get), categoria.Id, new { categoria });
+         }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+//         public async Task<IActionResult> Update(int id, [FromBody] Categoria categoria)
+//         {
+//             if (!ModelState.IsValid || id != categoria.Id)
+//             {
+//                 return BadRequest("Categoria informada com problemas");
+//             }
+
+//             if (!_db.Categorias.Any(c => c.Id == id))
+//             {
+//                 return NotFound("Categoria não encontrada!");
+//             }
+
+//             _db.Categorias.Update(categoria);
+//             await _db.SaveChangesAsync();
+
+//             return NoContent();
+//         }
+
+//         [HttpDelete("{id}")]
+//         [ProducesResponseType(StatusCodes.Status204NoContent)]
+//         [ProducesResponseType(StatusCodes.Status404NotFound)]
+//         public async Task<IActionResult> Delete(int id)
+//         {
+//             var categoria = await _db.Categorias.FindAsync(id);
+
+//             if (categoria == null)
+//             {
+//                 return NotFound("Categoria não encontrada!");
+//             }
+
+//             _db.Categorias.Remove(categoria);
+//             await _db.SaveChangesAsync();
+
+//             return NoContent();
+//         }
+//     }
+// }
